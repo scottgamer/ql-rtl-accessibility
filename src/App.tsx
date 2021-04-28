e513ddc7eb4f602cbff4c055eda7dbcebbce0a1c
rmunoz <richosojason@msn.com>
2021-04-28 17:25:45 -0500

    added basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UsersList from "./components/UsersList/UsersList";
 import UsersProvider from "./context/UsersProvider";
+import "./App.scss";
 
 function App() {
   return (
