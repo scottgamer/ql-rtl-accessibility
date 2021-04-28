b0503c991b8d5edffba764b3a0daac401af5f1da
rmunoz <richosojason@msn.com>
2021-04-28 17:10:10 -0500

    added msw config

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,13 @@
+import { rest } from "msw";
+import { User } from "../models/User";
+
+const mockedUsers: User[] = [
+  { id: 1, name: "Rich", username: "Rich" },
+  { id: 2, name: "Ariel", username: "Ariel" },
+];
+
+export const handlers = [
+  rest.get("https://jsonplaceholder.typicode.com/users", (req, res, ctx) => {
+    return res(ctx.json(mockedUsers));
+  }),
+];
