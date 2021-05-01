f5c3aa275bbb6d1548bec188d18dc85a30136e51
Richard Munoz <richosojason@msn.com>
2021-05-01 13:45:45 -0500

    finished tests and main app (#1)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,56 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("component has heading", () => {
+    render(<App />);
+    const title = screen.getByRole("heading", { name: "Acceptance form" });
+    expect(title).toHaveTextContent("Acceptance form");
+  });
+
+  test("checkbox starts unchecked", () => {
+    render(<App />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: "I accept the Terms and Conditions",
+    });
+    expect(checkbox).not.toBeChecked();
+  });
+
+  test("button starts disabled", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Continue" });
+    expect(button).toBeDisabled();
+  });
+
+  test("when checkbox is checked, enable button", () => {
+    render(<App />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: "I accept the Terms and Conditions",
+    });
+
+    fireEvent.click(checkbox);
+
+    const button = screen.getByRole("button", { name: "Continue" });
+    expect(button).toBeEnabled();
+  });
+
+  test("when button is enabled and clicked, change terms and conditions message", () => {
+    render(<App />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: "I accept the Terms and Conditions",
+    });
+
+    fireEvent.click(checkbox);
+
+    const button = screen.getByRole("button", { name: "Continue" });
+    fireEvent.click(button);
+
+    const paragraph = screen.getByText(
+      "Thanks for accepting the Terms and Conditions"
+    );
+    expect(paragraph).toBeInTheDocument();
+    expect(checkbox).not.toBeInTheDocument();
+    expect(button).not.toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const [continues, setContinues] = useState<boolean>(false);
+
   return (
     <div className="App">
-      <h1>React Testing Library &amp; Accessibility</h1>
+      <h1>Acceptance form</h1>
+
+      {!continues && (
+        <label>
+          <input
+            type="checkbox"
+            name="terms-and-conditions"
+            onClick={() => setDisabled(!disabled)}
+          />
+          I accept the Terms and Conditions
+        </label>
+      )}
+
+      {continues && <p>Thanks for accepting the Terms and Conditions</p>}
+
+      <br />
+      {!continues && (
+        <button disabled={disabled} onClick={() => setContinues(true)}>
+          Continue
+        </button>
+      )}
     </div>
   );
 }
